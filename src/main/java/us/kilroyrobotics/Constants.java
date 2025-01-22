4c25b937cb71219cd2c0d11dedca24646dcc8e14
Kilroy Programming <178730847+KilroyProgramming@users.noreply.github.com>
2025-01-21 20:13:51 -0500

    feat: toggle between high & low speeds with `x` button

---
+++ src/main/java/us/kilroyrobotics/Constants.java
@@ -0,0 +1,11 @@
+package us.kilroyrobotics;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
+import edu.wpi.first.units.measure.LinearVelocity;
+
+public final class Constants {
+    public static final class DriveConstants {
+        public static final LinearVelocity kLowDriveSpeed = MetersPerSecond.of(0.5);
+    }
+}
