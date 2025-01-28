9741575e25860a8af927c8db3f32ab1c10b8907e
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2025-01-28 08:14:21 -0500

    fix(elevator): set idle mode to brake

--- src/main/java/us/kilroyrobotics/subsystems/Elevator.java
+++ src/main/java/us/kilroyrobotics/subsystems/Elevator.java
@@ -15,6 +15,7 @@ import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Pose3d;
@@ -56,11 +57,13 @@ public class Elevator extends SubsystemBase {
                         ElevatorConstants.kI,
                         ElevatorConstants.kD,
                         ElevatorConstants.kF);
+        leadMotorConfig.idleMode(IdleMode.kBrake);
         leadMotorConfig.smartCurrentLimit(40);
         leadMotorConfig.encoder.positionConversionFactor(
                 ElevatorConstants.kEncoderPositionConversionFactor);
 
         SparkMaxConfig followerMotorConfig = new SparkMaxConfig();
+        followerMotorConfig.idleMode(IdleMode.kBrake);
         followerMotorConfig.smartCurrentLimit(40);
         followerMotorConfig.follow(this.m_leadMotor, true);
 
