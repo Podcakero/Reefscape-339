0bb947971e6905517a52fcf9f7dfc87ebfd3dfa0
Podcakero <github@podcake.net>
2025-02-14 13:10:44 -0500

    Remove extraneous import

--- src/main/java/us/kilroyrobotics/RobotContainer.java
+++ src/main/java/us/kilroyrobotics/RobotContainer.java
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
-import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import java.util.function.Supplier;
